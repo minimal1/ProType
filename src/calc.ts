e9a171906b30c800b436f0a4cc21bc0d2d60a22a
minimal1 <jcm940308@gmail.com>
2020-07-07 23:57:11 +0900

    Feature: add eventlistener into btns

--- src/calc.ts
+++ src/calc.ts
@@ -1,5 +1,23 @@
 /** @format */
 
-const outputDOM: HTMLElement = document.querySelector(".calcContainer");
+const container: HTMLDivElement = document.querySelector(".calcContainer");
 
-outputDOM.innerHTML = "Hello Guys";
+const output: HTMLInputElement = document.querySelector(".calc__output");
+const btns: NodeListOf<HTMLButtonElement> = document.querySelectorAll("button");
+
+const handleBtnClick: (event: MouseEvent) => void = (event) => {
+  const { target } = event;
+
+  console.log(target);
+};
+
+const init: () => void = () => {
+  btns.forEach((value: HTMLButtonElement) => {
+    value.addEventListener("click", handleBtnClick);
+  });
+  output.value = "0";
+};
+
+if (container) {
+  init();
+}
