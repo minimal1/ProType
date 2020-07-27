bc9bef738cce5234314b6e43080ea7981d02a9d6
minimal1 <jcm940308@gmail.com>
2020-07-28 01:07:58 +0900

    In progress: Todo done

--- src/Components/todo/TodoItem.tsx
+++ src/Components/todo/TodoItem.tsx
@@ -7,11 +7,24 @@ interface Todo {
   done: boolean;
 }
 
-const TodoItem = (todo: Todo) => (
-  <TodoContainer>
-    <span>{todo.text}</span>
-  </TodoContainer>
-);
+interface IProps {
+  todo: Todo;
+  onChange: ({ id, text, done }: Todo) => void;
+}
+
+const TodoItem: React.FC<IProps> = ({ todo, onChange }: IProps) => {
+  const handleChange = () => {
+    console.log('here', { id: todo.id, text: todo.text, done: !todo.done });
+
+    onChange({ id: todo.id, text: todo.text, done: !todo.done });
+  };
+  return (
+    <TodoContainer>
+      <input type="checkbox" checked={todo.done} onChange={handleChange} />
+      <span>{todo.text}</span>
+    </TodoContainer>
+  );
+};
 
 const TodoContainer = styled.div``;
 
--- src/Components/todo/Todolist.tsx
+++ src/Components/todo/Todolist.tsx
@@ -7,14 +7,69 @@ interface Todo {
   done: boolean;
 }
 
-const Todolist = () => {
-  const [todos, setTodos] = React.useState<Todo[]>([]);
+interface IProps {
+  _: null;
+}
 
+const Todolist: React.FC<IProps> = () => {
+  // const [todos, setTodos] = React.useState(new Map<string, Todo>());
+  const [todos, setTodos] = React.useState<Todo[]>([]);
+  const [text, setText] = React.useState<string>('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  const changeTodo = ({ id, text, done }: Todo) => {
+    setTodos(
+      todos.map(todo => {
+        return todo.id === id ? { id, text, done } : todo;
+      }),
+    );
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const newTodo: Todo = {
+      id: Date.now().toString(),
+      text,
+      done: false,
+    };
+    setTodos([...todos, newTodo]);
+    setText('');
+  };
+  console.log('render');
+
+  // const todosUI = [];
+
+  // todos.forEach((v, k) =>
+  //   // todosUI.push(<TodoItem key={k} todo={v} onChange={changeTodo} />),
+  //   todosUI.push(
+  //     <div key={k}>
+  //       <input
+  //         type="checkbox"
+  //         checked={v.done}
+  //         onChange={() => changeTodo({ id: v.id, text: v.text, done: !v.done })}
+  //       />
+  //       <span>{v.text}</span>
+  //     </div>,
+  //   ),
+  // );
   return (
     <div>
+      <h2>Todo List</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={text} onChange={handleChange} />
+      </form>
+      <br />
+      {/* {Array.from(todos, ([key, todo]) => {
+        console.log('Key:', key, 'todo:', { ...todo });
+        return <TodoItem key={key} todo={todo} onChange={changeTodo} />;
+      })} */}
       {todos.map(todo => (
-        <TodoItem todo />
+        <TodoItem key={todo.id} todo={todo} onChange={changeTodo} />
       ))}
+      {/* {todosUI[0]} */}
     </div>
   );
 };
